d741c02b534e696b1e46cf661c602da25e7fde01
joyant-thinkcrypt <joyant.thinkcrypt@gmail.com>
2025-09-10 12:27:16 +0600

    added category section changes

--- src/components/HomePage/ProductSection.tsx
+++ src/components/HomePage/ProductSection.tsx
@@ -6,32 +6,31 @@ import CustomContainer from '../reusable/Container';
 import { DetailedProduct } from '../data/productData';
 
 interface ProductSectionProps {
-  title: string;
-  products: DetailedProduct[];
+	title: string;
+	products: DetailedProduct[];
 }
 
 const ProductSection: React.FC<ProductSectionProps> = ({ title, products }) => (
-  <CustomContainer pb={10}>
-    <SectionHeader2 title={title} />
-    <Grid
-      templateColumns={{
-        base: '1fr',
-        sm: 'repeat(2, 1fr)',
-        md: 'repeat(2, 1fr)',
-        lg: 'repeat(3, 1fr)',
-        '2xl': 'repeat(6, 1fr)',
-      }}
-      gap={6}
-    >
-      {products.slice(0, 6).map((product, index) => (
-        <ProductCard
+	<CustomContainer pb={10}>
+		<SectionHeader2 title={title} />
+		<Grid
+			templateColumns={{
+				base: '1fr',
+				sm: 'repeat(2, 1fr)',
+				md: 'repeat(2, 1fr)',
+				lg: 'repeat(3, 1fr)',
+				'2xl': 'repeat(6, 1fr)',
+			}}
+			gap={6}
+		>
+			{products.slice(0, 6).map((product, index) => (
+				<ProductCard
 					key={index}
 					product={product} // only this prop
 				/>
-
-      ))}
-    </Grid>
-  </CustomContainer>
+			))}
+		</Grid>
+	</CustomContainer>
 );
 
 export default ProductSection;
